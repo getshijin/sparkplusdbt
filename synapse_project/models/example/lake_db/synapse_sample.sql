af82de143274cae453af85141125fa18916d216b
Shijin Mohanan Sheela <getshijin@gmail.com>
2026-01-17 19:15:09 +0100

    17/01/26 ver3

--- synapse_project/models/example/lake_db/synapse_sample.sql
+++ synapse_project/models/example/lake_db/synapse_sample.sql
@@ -3,12 +3,12 @@ with source_store_trans as (
     select *
     from {{ source('lake_db', 'ext_transactions') }}
 
-)
+),
 
-final as (
+final_1 as (
     select *
     from source_store_trans
 )
 
 select *
-from final
+from final_1
